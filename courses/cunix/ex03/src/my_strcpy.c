c26c0ee8ff583b7d03fff2029c15b5365dd1daf1
Bohdan Panamarenko <panamarenkobohdan@gmail.com>
2021-08-11 13:17:25 +0000

    Ex03 solution

---
+++ courses/cunix/ex03/src/my_strcpy.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+char *my_strcpy(char *dest, const char *src)
+{
+    char *dest_to_return = dest; // save pointer to a start of dest
+    while (*src != '\0') 
+    {
+        *(dest++) = *(src++);
+    }
+    *(dest) = '\0';
+    return dest_to_return;
+}
